dc7754061d2ef323da6f003cf265e27bc8d3ec3b
Laurentiu Spilca <laurentiu.spilca@endava.com>
2018-11-21 20:22:29 +0200

    Initial commit

---
+++ src/main/java/com/endava/devtalksjr2018/Devtalksjr2018Application.java
@@ -0,0 +1,12 @@
+package com.endava.devtalksjr2018;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Devtalksjr2018Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Devtalksjr2018Application.class, args);
+	}
+}
